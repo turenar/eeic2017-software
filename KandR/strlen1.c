accdbe01d36e95a0d69435a0c4eda211d0ce1fb6
AkariAsai <akari.1311aquamarine4@gmail.com>
2016-10-20 16:36:11 +0900

    added another program example on strlen function and comments.

--- KandR/strlen1.c
+++ KandR/strlen1.c
@@ -3,10 +3,19 @@
 int strlen1(char *s){
   char *p = s;
 
-  while (*p != '\0'){
+  while (*p != '\0'){//文字列の最後になるまでp++をインクリメントする。つまりの次の文字を指ししめす。
     p++;
   }
-  return p - s;
+  return p - s;//sで奨めれられた文字数、つまり文字列の長さが与えられる。
+}
+
+//別解。こちらの方が直感的に理解しやすいかもしれないです。
+int strlen2(char *s){
+  int n;
+
+  for (n = 0; *s != '\0'; s++)
+    n++;
+  return n;
 }
 
 
