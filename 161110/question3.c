9b068f8c25d2b38be2c493f2749c576fd22ef0cd
Turenar <sora@turenar.xyz>
2016-11-15 23:41:29 +0900

    コメント更新

--- 161110/question3.c
+++ 161110/question3.c
@@ -6,6 +6,6 @@ int main() {
   a = &b;
   printf("%d\n", *a); // output: 10
   //ちなみにもちろん以下もオッケーですが問題の趣旨的に多分ポインタを使う方が正しいかと。
-  printf("%d\n", *a); // output: 10
+  printf("%d\n", b); // output: 10
   return 0;
 }
--- 161110/question4.c
+++ 161110/question4.c
@@ -7,9 +7,9 @@ int main() {
   /*
      "hello"[1]
      は"hello"というchar配列の2番目（注）の要素、つまり'e'を返します。
-     char'd'から-1しているので,ASCIIテーブルでcharの一つまえ、つまり小文字のdが帰ってきます。
+     char'e'から-1しているので,ASCIIテーブルでcharの一つまえ、つまり小文字のdが帰ってきます。
      ASCII表: http://www9.plala.or.jp/sgwr-t/c_sub/ascii.html
-     （注）Cは0-indexing、つまり0から数え上げが始まります。うっかり最初の要素だと思いわないように注意。
+     （注）Cは0-indexing、つまり0から数え上げが始まります。うっかり最初の要素だと思わないように注意。
    */
   return 0;
 }
